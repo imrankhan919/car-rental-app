cb3d683843027492cefe98dfa792fbcf018f7b8b
imrankhan919 <imrankhan919.ik@gmail.com>
2025-04-02 18:55:10 +0530

    rental_updated

--- server/controllers/rentController.js
+++ server/controllers/rentController.js
@@ -2,13 +2,40 @@ const expressAsyncHandler = require("express-async-handler");
 const Car = require("../models/carModel");
 const Rental = require("../models/rentalModel");
 
-const getUserRentals = async (req, res) => {
-  res.send("All Users Rentals!!");
-};
+function getDateDifference(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
 
-const getUserRental = async (req, res) => {
-  res.send("Single Rental!!");
-};
+  if (isNaN(start) || isNaN(end)) {
+    throw new Error("Invalid date format");
+  }
+
+  const timeDifference = end - start;
+  const daysDifference = timeDifference / (1000 * 3600 * 24);
+  return daysDifference;
+}
+
+const getUserRentals = expressAsyncHandler(async (req, res) => {
+  const rentals = await Rental.find({ user: req.user._id });
+
+  if (!rentals) {
+    res.status(404);
+    throw new Error("Rentals Not Found!");
+  }
+
+  res.status(200).json(rentals);
+});
+
+const getUserRental = expressAsyncHandler(async (req, res) => {
+  const rental = await Rental.findById(req.params.rid);
+
+  if (!rental) {
+    res.status(404);
+    throw new Error("Rental Not Found!");
+  }
+
+  res.status(200).json(rental);
+});
 
 const addUserRental = expressAsyncHandler(async (req, res) => {
   const { pickupDate, dropDate } = req.body;
@@ -26,9 +53,17 @@ const addUserRental = expressAsyncHandler(async (req, res) => {
     throw new Error("Invalid Car Request");
   }
 
-  //  Fix This
-  let totalBill =
-    (dropDate.split("-")[0] - pickupDate.split("-")[0]) * carExist.rate;
+  let totalBill = getDateDifference(pickupDate, dropDate) * carExist.rate;
+
+  console.log(totalBill);
+
+  // Check if car is available
+  const car = await Car.findById(req.params.cid);
+
+  if (car.isBooked) {
+    res.status(400);
+    throw new Error("Car is already booked!!");
+  }
 
   const newRental = {
     user: req.user._id,
@@ -58,8 +93,30 @@ const addUserRental = expressAsyncHandler(async (req, res) => {
   });
 });
 
-const updateRental = async (req, res) => {
-  res.send("Rental Updated!!");
-};
+const updateRental = expressAsyncHandler(async (req, res) => {
+  const { dropDate } = req.body;
+
+  if (!dropDate) {
+    res.status(400);
+    throw new Error("Kindly Add Drop Date");
+  }
+
+  const rental = await Rental.findById(req.params.rid);
+  const car = await Car.findById(rental.car);
+
+  const newBill = getDateDifference(rental.pickupDate, dropDate) * car.rate;
+
+  const updatedRental = await Rental.findByIdAndUpdate(req.params.rid, {
+    dropDate: dropDate,
+    totalBill: newBill,
+  });
+
+  if (!updatedRental) {
+    res.status(400);
+    throw new Error("Rental Not Updated");
+  }
+
+  res.status(200).json(updatedRental);
+});
 
 module.exports = { getUserRentals, addUserRental, getUserRental, updateRental };
