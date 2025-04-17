f1d7f128f8fb227e0efcc351ce16da986ad033ba
Huzaifa Mill <huzefafm53786@gmail.com>
2025-04-17 22:19:05 +0530

    file upload changes

--- server/middleware/multerMiddleware.js
+++ server/middleware/multerMiddleware.js
@@ -1,8 +1,14 @@
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp")
+        const dir = path.join(__dirname, '../public/temp');
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true }); // Create the directory if it doesn't exist
+        }
+        cb(null, dir);
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
